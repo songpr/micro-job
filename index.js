06b8c183f862f46314a27bcb66f5d53c50ed357f
Pornchai.Rungsirijaratthong <Pornchai.Rungsirijaratthong@wundermanthompson.com>
2022-02-01 21:33:19 +0700

    support async

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
 'use strict'
-
+const { isAsyncFunction } = require('util/types')
 class MicroJob {
   /**
    *
    * @param {function} jobHandler - function to handle a batch of data for this job
    * @param {Number} msToRun - millisecond to run a batch of data using the handler
    */
-  constructor(jobHandler, msToRun) {
+  constructor (jobHandler, msToRun) {
     if (typeof (jobHandler) !== 'function') throw Error('job handler is not a function.')
     if (!Number.isInteger(msToRun) || msToRun < 1) throw Error('invalid milliseconds to run job ')
     const _jobHandler = jobHandler
     const _msToRun = msToRun
     let timeout = null
     let dataItems = []
-    const runAbatch = () => {
-      const currentDataItems = dataItems
-      dataItems = []
-      // const currenttimeout = timeout
-      timeout = null
-      if (currentDataItems.length > 1) _jobHandler(currentDataItems)
-    }
+    let error = null
+    Object.defineProperty(this, 'lastError', {
+      get: () => error
+    })
+    Object.defineProperty(this, 'isABatchScheduled', {
+      get: () => timeout != null
+    })
+    const runAbatch = isAsyncFunction(_jobHandler)
+      ? () => {
+          const currentDataItems = dataItems
+          dataItems = []
+          const currenttimeout = timeout
+          timeout = null
+          if (currentDataItems.length > 1) {
+            _jobHandler(currentDataItems).then(() => {
+              error = null
+            }).catch(err => {
+              error = Object.freeze({ error: err, data: currentDataItems, timeout: currenttimeout })
+            })
+          }
+        }
+      : () => {
+          const currentDataItems = dataItems
+          dataItems = []
+          const currenttimeout = timeout
+          timeout = null
+          if (currentDataItems.length > 1) {
+            try {
+              _jobHandler(currentDataItems)
+              error = null
+            } catch (err) {
+              error = Object.freeze({ error: err, data: currentDataItems, timeout: currenttimeout })
+            }
+          }
+        }
 
     const addData = (data) => {
       if (data === undefined) throw Error('can not add undefined as data')
--- test/job.test.js
+++ test/job.test.js
@@ -15,22 +15,47 @@ test('basic micro job', async t => {
     t.ok(Array.isArray(items), 'data passed must be array')
     t.ok(items.length > 0, 'data array length must more that 0')
   }
-  const itemsTestJob = new MJob(itemsTest, 51)
+  const itemsTestJob = new MJob(itemsTest, 10)
   let i = 0
   for (; i < 20; i++) itemsTestJob.addData(i)
-  await setTimeoutAsync(100, '1st batch')
+  await setTimeoutAsync(20, '1st batch')
   for (; i < 60; i++) itemsTestJob.addData(i)
-  await setTimeoutAsync(100, '2nd batch')
+  await setTimeoutAsync(20, '2nd batch')
 })
 
 test('basic micro job, data is null can be put as data - but not undefined ', async t => {
   const itemsTest = items => {
+    console.log({ items })
+    t.ok(Array.isArray(items), 'data passed must be array')
+    t.ok(items.length > 0, 'data array length must more that 0')
+    items.map(item => {
+      if (item === null) {
+        throw Error('do not support null item data in test')
+      }
+    })
+  }
+  const itemsTestJob = new MJob(itemsTest, 10)
+  itemsTestJob.addData(null)
+  t.ok(itemsTestJob.isABatchScheduled)
+  await setTimeoutAsync(50, '1st batch done')
+  console.log({ lastError: itemsTestJob.lastError })
+  t.ok(itemsTestJob.lastError, 'when the batch is fail to run the error will be captured in lastError')
+  t.throws(() => itemsTestJob.addData(undefined), 'cannot add undefined data')
+})
+
+test('basic micro job, run async function handler', async t => {
+  const itemsTest = async (items) => {
+    const result = await setTimeoutAsync(10, items.reduce((total, num) => total + num, 0))
+    console.log({ result })
+    t.ok(Number.isInteger(result))
     t.ok(Array.isArray(items), 'data passed must be array')
     t.ok(items.length > 0, 'data array length must more that 0')
   }
-  const itemsTestJob = new MJob(itemsTest, 51)
+  const itemsTestJob = new MJob(itemsTest, 10)
   itemsTestJob.addData(null)
-  await setTimeoutAsync(100, '1st batch')
+  await setTimeoutAsync(10, 'wait for 1st batch')
   t.throws(() => itemsTestJob.addData(undefined), 'cannot add undefined data')
-  await setTimeoutAsync(100, '2nd batch')
+  let i = 0
+  for (; i < 20; i++) itemsTestJob.addData(i)
+  await setTimeoutAsync(50, 'wait for 2nd batch')
 })
